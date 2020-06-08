73a9dae4fa43895262d6d8be4753d29ccad5718b
victormvila <victormvilavillanueva@gmail.com>
2020-06-08 18:32:17 +0200

    Añadida version V2 de OrganizaDescargas

--- OrganizaDescargas.py
+++ OrganizaDescargas.py
@@ -24,6 +24,24 @@ import time
 import sys
 import getpass
 
+try:
+    from zipfile import ZipFile
+except ImportError: 
+    print("Error. ZipFile module is required")
+    time.sleep(1)
+    print("Execute 'pip install zipfile' in cmd")
+    time.sleep(1)
+    exit()
+
+try:
+    from rarfile import RarFile
+except ImportError:
+    print("Error. RarFile module is required")
+    time.sleep(1)
+    print("Execute 'pip install rarfile' in cmd")
+    time.sleep(1)
+    exit()
+
 # Se cierran clientes P2P que puedan tener ficheros en uso del directorio a organizar.
 os.chdir("C:/Windows/system32")
 if not os.system("taskkill /f /im qbittorrent.exe") == 128:
@@ -37,15 +55,12 @@ rutaDescargas = "C:/Users/" + nombreUsuario + "/Downloads"
 nombreCarpetaVideos = "Peliculas"
 pathCarpetaVideos = "C:/Users/" + nombreUsuario + "/Desktop/" + nombreCarpetaVideos
 pathCarpetaDocumentos = "C:/Users/" + nombreUsuario + "/Desktop/Documentos"
-pathCarpetaFichComprimidos = "C:/Users/" + nombreUsuario + "/Desktop/FicherosComprimidos"
 
 # Si no existen los directorios que guardarán los ficheros de Descargas, se crean
 if not os.path.isdir(pathCarpetaVideos):
     os.mkdir(pathCarpetaVideos)    
 if not os.path.isdir(pathCarpetaDocumentos):
     os.mkdir(pathCarpetaDocumentos) 
-if not os.path.isdir(pathCarpetaFichComprimidos):
-    os.mkdir(pathCarpetaFichComprimidos)
 
 print("#########################")
 print("#######BIENVENIDO########")
@@ -81,8 +96,12 @@ for dirpath, dirnames, filenames in os.walk(rutaDescargas):
             time.sleep(1)
             #El modulo shutil permite realizar operaciones con ficheros.
             #Recibe el path str (ruta de origen del fichero) y el path dst (ruta de destino)
-            shutil.move(os.path.abspath(f),pathCarpetaVideos)
-            print("Se ha movido el fichero " + f + " a la carpeta Videos")
+            try:
+                shutil.move(os.path.abspath(f),pathCarpetaVideos)
+                print("Se ha movido el fichero " + f + " a la carpeta Videos")
+            except shutil.Error:
+                print("El archivo ya existe en el directorio de destino")
+            
             time.sleep(1)
         if (extension==".pdf" or extension==".doc" or extension==".docx" or extension==".odt" 
             or extension==".xls" or extension==".xlsx" or extension=="pptx" or extension=="ppt"):
@@ -98,8 +117,22 @@ for dirpath, dirnames, filenames in os.walk(rutaDescargas):
             print("#######FICHERO COMPRIMIDO DETECTADO#######")
             print("##########################################")
             time.sleep(1)
-            shutil.move(os.path.abspath(f),pathCarpetaFichComprimidos)
-            print("Se ha movido el fichero " + f + " a la carpeta de ficheros comprimidos")
+            print("Contenido:")
+            time.sleep(1)
+            if extension==".rar":
+                try:
+                    os.rename(f,nombre+".zip")
+                except FileExistsError:
+                    print("Se intenta renombrar un archivo cuyo nombre corresponde al de otro ya existente")
+                
+            with ZipFile(f,'r') as zip:
+                zip.printdir()
+                time.sleep(1)
+                print("Extrayendo archivos")
+                zip.extractall(nombre)
+                time.sleep(1)
+                print("Extracción completada")
+                
             time.sleep(1)
             
 print("")
@@ -132,3 +165,4 @@ while not end:
     else: 
         print("Orden inválida. Introduzca de nuevo [Y/N]: ", end="")
         orden=input()
+        
